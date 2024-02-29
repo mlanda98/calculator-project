5607c62f56f3455e2ff784851ad50cca65e34b46
Mayra Landa <mlandae16@gmail.com>
2024-02-29 12:29:37 -0800

    delete console.logs
    deleted for the add, subtract, divide, multipy functions

--- script.js
+++ script.js
@@ -2,23 +2,17 @@ function add(a, b){
   return a + b;
 };
 
-console.log(add(2, 2));
-
 function subtract(a, b){
   return a - b;
 };
-console.log(subtract(4 ,2));
 
 function multiply(a ,b){
   return a * b;
 };
 
-console.log(multiply(2,2));
-
 function divide(a, b){
   return a / b;
 };
-console.log(divide(2, 2));
 
 let num1 = 0;
 let num2 = 0;
