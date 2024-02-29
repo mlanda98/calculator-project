45dfba37f096841d41a65d209c7e565ff1c69c56
Mayra Landa <mlandae16@gmail.com>
2024-02-28 17:37:53 -0800

    make a function that operates 2 numbers and an operator and gives a result

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ let num1 = 0;
 let num2 = 0;
 let operator = "";
 
+function operate(num1, num2, operator){
+  if (operator === "+"){
+    return num1 + num2;
+  } else if(operator === "-"){
+    return num1 - num2;
+  } else if (operator === "*"){
+    return num1 * num2;
+  } else if (operator === "/"){
+    return num1 / num2;
+  }
+}
+
+let result = operate(2,2, "+");
+console.log(result);
