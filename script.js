74f6c81e77f66d8a5ede1c73d9251ae5ea3be9fc
Mayra Landa <mlandae16@gmail.com>
2024-02-29 16:32:18 -0800

    make functions to display each number of each button

--- script.js
+++ script.js
@@ -32,3 +32,70 @@ function operate(a, b, operator){
 
 let result = operate(2,2, "+");
 console.log(result);
+
+
+
+document.getElementById("one").addEventListener("click", displayOne)
+function displayOne(){
+  let buttonText = 1;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+};
+
+document.getElementById("two").addEventListener("click", displayTwo)
+function displayTwo(){
+  let buttonText = 2;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+};
+
+document.getElementById("three").addEventListener("click", displayThree)
+function displayThree(){
+  let buttonText = 3;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+};
+
+document.getElementById("four").addEventListener("click", displayFour)
+function displayFour(){
+  let buttonText = 4;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+};
+
+document.getElementById("five").addEventListener("click", displayFive)
+function displayFive(){
+  let buttonText = 5;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+};
+
+document.getElementById("six").addEventListener("click", displaySix)
+function displaySix(){
+  let buttonText = 6;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+}
+
+document.getElementById("seven").addEventListener("click", displaySeven)
+function displaySeven(){
+  let buttonText = 7;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+};
+
+
+
+document.getElementById("eight").addEventListener("click", displayEight)
+function displayEight(){
+  let buttonText = 8;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+};
+
+document.getElementById("nine").addEventListener("click", displayNine)
+function displayNine(){
+  let buttonText = 9;
+  let numberDiv = document.getElementById("display");
+  numberDiv.innerText = buttonText;
+};
