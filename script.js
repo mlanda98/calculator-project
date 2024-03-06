a2472b054938dbd820f25b9178c79127b30c0854
Mayra Landa <mlandae16@gmail.com>
2024-03-05 16:59:23 -0800

    finish calculator project
    It calculates multiple numbers and clear data

--- script.js
+++ script.js
@@ -1,61 +1,84 @@
-function add(a, b){
-  return a + b;
-};
+let previousNum = "";
+let currentNum = "";
+let currentOperator = "";
 
-function subtract(a, b){
-  return a - b;
-};
-
-function multiply(a ,b){
-  return a * b;
-};
-
-function divide(a, b){
-  return a / b;
-};
-
-let num1 = 0;
-let num2 = 0;
-let operator = "";
-
-function operate(a, b, operator){
-  if (operator === "+"){
-    return add(a, b);
-  } else if(operator === "-"){
-    return subtract(a, b);
-  } else if (operator === "*"){
-    return multiply(a, b);
-  } else if (operator === "/"){
-    return divide(a, b);
+function calculate(){
+  let previous = parseFloat(previousNum);
+  let current = parseFloat(currentNum);
+  let result;
+  if (currentOperator === "+"){
+    result = previous + current;
+  } else if (currentOperator === "-"){
+    result = previous - current;
+  } else if (currentOperator === "x"){
+    result = previous * current;
+  } else if (currentOperator === "/"){
+    if (current === 0){
+      result = "Error: Division by zero"
+    } else {
+      result = previous / current;
+    }
   }
+
+  currentNum = result;
+  previousNum = "";
+
 }
 
-let result = operate(2,2, "+");
-console.log(result);
-
-let a = " ";
-let b = " ";
 
 const buttonNumbers = document.querySelectorAll("#number-button");
 
 buttonNumbers.forEach(button => {
   button.addEventListener("click", function(){
-    if (a === " "){
-      a = button.textContent;
-    displayValue1();
-    } else if (b === " "){
-      b = button.textContent;
-    displayValue2();
-    } 
-  });
+      if (currentNum === "0"){
+        currentNum = "";
+      };
+      currentNum += (button.innerText);
+      displayValue();
+
+    });
 });
 
-function displayValue1(){
-  document.getElementById("display").innerHTML = + a ;
-}
-function displayValue2(){
-  document.getElementById("display").innerHTML = + b ;
- 
+const operatorButtons = document.querySelectorAll("#operator");
+
+operatorButtons.forEach(operatorButton => {
+  operatorButton.addEventListener("click", function (){
+    operate();
+    currentOperator = (operatorButton.innerText);
+    currentNum ="";
+    displayValue();
+    
+
+  })
+});
+
+const equal = document.getElementById("equal");
+equal.addEventListener("click", function(){
+      calculate();
+      displayValue();
+});
+
+const display = document.getElementById("display");
+const displayRow = document.getElementById("display2");
+function displayValue(){
+  document.getElementById("display").innerHTML = currentNum;
+  document.getElementById("display2").innerHTML = previousNum;
 }
 
+function operate(){
+  if (currentNum == "") return;
+  if (previousNum !== "" && currentOperator !== ""){
+    calculate();
+  }
+  previousNum = currentNum + currentOperator;
+  currentNum = "";
+}
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", function(){
+  currentNum = "";
+  previousNum = "";
+  operation = "";
+  displayValue();
+})
 
